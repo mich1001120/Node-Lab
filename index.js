0d48dbac12089f07717271037ddb8e9031c8895b
mich1001120 <mich1001120@gmail.com>
2017-02-27 15:01:05 +0800

    在本機指定的位置開啟預設上傳的圖檔

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var handle = {}; //建立一個handle陣列來彙整所有的handler
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
