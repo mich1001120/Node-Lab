8cb5f55f910085bd3f103b8ad2f4d9d999c4f0e9
mich1001120 <mich1001120@gmail.com>
2017-02-27 14:31:53 +0800

    在/start頁面輸入，在/upload呈現結果

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response){
+function route(handle, pathname, response, postData){
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') { //確定回傳的函數是否存在，存在就執行
-        handle[pathname](response);
+        handle[pathname](response, postData);
     }else{
         console.log("No request handler found for " + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
