854958a578a48ebecf3c77eee7398dc39dec9b14
mich1001120 <mich1001120@gmail.com>
2017-02-27 10:48:25 +0800

    把reponse的參數移到router裡，修正非同步的效果

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,22 @@
 var exec = require("child_process").exec;
 
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
-  var content = "empty";
 
-  exec("ls -lah", function (error, stdout, stderr) {
-    content = stdout;
-  });
-
-  return content;
+  exec("find /",
+    { timeout: 10000, maxBuffer: 20000*1024 }, //測試非同步效果，沒看到測試結果
+    function (error, stdout, stderr) {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write(stdout);
+      response.end();
+    });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname){
+function route(handle, pathname, response){
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') { //確定回傳的函數是否存在，存在就執行
-        return handle[pathname]();
+        handle[pathname](response);
     }else{
         console.log("No request handler found for " + pathname);
-        return "404 Not found";
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404 Not found");
+        response.end();
     }
 }
 
